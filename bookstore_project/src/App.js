9654fa1a5a01b387dd5f33de3081bc423a5643dd
Davdu <92082406+Davdu@users.noreply.github.com>
2023-05-12 15:00:37 +0200

    Icons installed

--- bookstore_project/src/App.js
+++ bookstore_project/src/App.js
@@ -1,9 +1,21 @@
+import Grid from '@mui/material/Unstable_Grid2';
+import navBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 function App() {
   return (
-    <Button variant="contained">Hello</Button>
+    <div>
+      <Grid >
+        <navBar>
+          <Toolbar >
 
+          </Toolbar>
+        </navBar>
+      </Grid>
+    </div>
+    
   );
 }
 
