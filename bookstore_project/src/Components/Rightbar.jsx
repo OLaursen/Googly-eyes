8cb18f4672264a9b4cd689c452dc0ba9aaa6f3bd
OLaursen <olau@itu.dk>
2023-05-19 12:22:30 +0200

    Lol

--- bookstore_project/src/Components/Rightbar.jsx
+++ bookstore_project/src/Components/Rightbar.jsx
@@ -3,7 +3,7 @@ import React from "react"
 
 const Rightbar = () => {
     return(
-        <Box bgcolor={"purple"} flex={1} p={2} sx={{display: {xs: "none", sm: "block"}}}>Rightbar</Box>
+        <Box bgcolor={"purple"} flex={1} p={2} sx={{display: {xs: "none", sm: "block"}}}>Right bar</Box>
     )
 }
 
