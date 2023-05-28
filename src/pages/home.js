9b7fad23645b6e09a6fc47379e346aae54b91d74
OLaursen <olau@itu.dk>
2023-05-29 01:33:30 +0200

    Functionality to createListing

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Box, Stack } from '@mui/material';
 import Sidebar from '../Components/SideBar';
 import Feed from '../Components/ContentGrid';
-import Rightbar from '../Components/RightBar/index';
+
 import NavBar from '../Components/NavBar/NavBar';
 
 export default class Home extends React.Component {
@@ -14,7 +14,7 @@ export default class Home extends React.Component {
                     <Stack direction="row" spacing={2} justifyContent="space-between">
                         <Sidebar />
                         <Feed />
-                        <Rightbar />
+                       
                     </Stack>
                 </Box>
             </>
