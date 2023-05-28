a1bcd779c36049bd55fcef86de3420cb03a2fa43
Mikkel <110497736+MikkelBKristensen@users.noreply.github.com>
2023-05-28 20:14:25 +0200

    Make NavBar not appear on signin/signup

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,11 +3,13 @@ import { Box, Stack } from '@mui/material';
 import Sidebar from '../Components/SideBar';
 import Feed from '../Components/ContentGrid';
 import Rightbar from '../Components/RightBar/index';
+import NavBar from '../Components/NavBar/NavBar';
 
 export default class Home extends React.Component {
     render() {
         return (
-            <div>
+            <>
+                <NavBar />
                 <Box sx={{ background: "#fff"}} >
                     <Stack direction="row" spacing={2} justifyContent="space-between">
                         <Sidebar />
@@ -15,7 +17,7 @@ export default class Home extends React.Component {
                         <Rightbar />
                     </Stack>
                 </Box>
-            </div>
+            </>
         )
     }
 }
