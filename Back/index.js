4e12b3e3eb278499a643d2e6da650ad258e1e113
Qezert <bekr@itu.dk>
2023-05-29 23:57:40 +0200

    Refactoring to Front and Back folders

--- Back/index.js
+++ Back/index.js
@@ -1,9 +1,11 @@
-import express, { response } from "express";
+import express from "express";
 import cors from "cors";
 import fileUpload from "express-fileupload";
 import { createRandomID } from "./utility.js";
 import * as books from "./books/books.model.js";
 import * as users from "./users/users.model.js"
+import * as rooms from "./rooms/rooms.model.js"
+import * as bookings from './bookings/bookings.model.js'
 
 const app = express();
 const PORT = 3001; // fetch url: localhost:3001
@@ -35,35 +37,7 @@ app.post("/upload", (request, response) => {
 
 // vJERES KODE HERv
 
-app.get('/book', async (request, response) => {
-  const booksData = await books.getAll();
-  response.send(JSON.stringify(booksData));
-  console.log(booksData);
-})
 
-app.get('/book/:id', async (request, response) => {
-
-  const id = request.params.id;
-
-  const bookData = await books.getByID(id);
-  response.send(JSON.stringify(bookData));
-  console.log(bookData);
-})
-
-app.post('/book', async (request, response) => {
-  books.add(request.body);
-  response.send("Book Added");
-})
-
-app.delete('/book/:id', async (request, response) => {
-  
-  const bookId = request.params.id;
-
-  books.remove(bookId);
-
-  response.send("Book Deleted");
-
-})
 
 // ^JERES KODE HER^
 
